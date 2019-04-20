1f699c9e6b7b6fab25f1c56c6871669b219b3a30
Tomoki Okumura <tmkokumura@gmail.com>
2019-04-20 23:18:38 +0900

    Remove unnecessary files.

--- README.md
+++ README.md
@@ -1,10 +1,35 @@
-# Train
-## Simple training of LightGBM model.
+# How to use
+## Git checkout
+```
+git checkout git@github.com:tmkokumura/kaggle-home-credit-default-risk.git
+```
+
+## Install depending modules
+```
+pip install -f requirements.txt
+```
+
+## Preprocess
+
+
+## Train
+### Simple training of LightGBM model.
 ```
 python train_lgbm.py
 ```
 
-## Cross validation training of LightGBM model.
+### Cross validation training of LightGBM model.
 ```
 python train_lgbm_cv.py
+```
+
+
+### Grid search of LightGBM model.
+```
+python train_lgbm_gs.py
+```
+
+###  Simple training of DNN model.
+```
+python train_nn.py
 ```
--- train_nn.py
+++ train_nn.py
@@ -11,10 +11,9 @@ from tqdm import tqdm
 from statistics import mean
 import numpy as np
 import gc
+from logging
+
 # import utils
-
-from logging import getLogger, StreamHandler, Formatter, FileHandler, DEBUG, INFO
-
 TRAIN_FILE = '../input/custom/201808231336_train_norm_debug.csv'
 TEST_FILE = '../input/custom/201808231336_test_norm_debug.csv'
 SUBMISSION_FILE = '../result/submission.csv'
@@ -23,17 +22,10 @@ LOG_FILE = '../log/train_nn.py.log'
 KEY_COL = 'SK_ID_CURR'
 TGT_COL = 'TARGET'
 
-# Log Settings
-logger = getLogger(__name__)
-log_fmt = Formatter('%(asctime)s %(name)s %(lineno)d [%(levelname)s][%(funcName)s] %(message)s')
-log_handler = StreamHandler()
-log_handler.setLevel(INFO)
-log_handler.setFormatter(log_fmt)
-logger.addHandler(log_handler)
-log_handler = FileHandler(LOG_FILE, 'a')
-log_handler.setFormatter(log_fmt)
-logger.setLevel(DEBUG)
-logger.addHandler(log_handler)
+# Logger Settings.
+fmt = '%(asctime)s %(levelname)s [%(module)s#%(funcName)s] :%(message)s'
+logging.basicConfig(level=logging.DEBUG, format=fmt)
+logger = logging.getLogger('Main')
 
 def gen_model():
     """
@@ -76,55 +68,58 @@ def gen_model():
 
     model.compile(loss=loss, optimizer=Adam(lr=lr, beta_1=beta_1, beta_2=beta_2), metrics=[metrics])
     return model
+    
 
-logger.info('=== start nn.py ===')
+if __name__ == '__main__':
 
-logger.info('--- load data ---')
-df_train = pd.read_csv(TRAIN_FILE).sort_values(by=KEY_COL).reset_index(drop=True)
-logger.info('df_train: {}'.format(df_train.shape))
-df_test = pd.read_csv(TEST_FILE).sort_values(by=KEY_COL).reset_index(drop=True)
-logger.info('df_test: {}'.format(df_test.shape))
+	logger.info('=== start nn.py ===')
 
-logger.info('--- X-Y split ---')
-x_train = df_train.drop(columns=[KEY_COL, TGT_COL])
-logger.info('x_train: {}'.format(x_train.shape))
-y_train = df_train[TGT_COL]
-logger.info('y_train: {}'.format(y_train.shape))
-del df_train
+	logger.info('--- load data ---')
+	df_train = pd.read_csv(TRAIN_FILE).sort_values(by=KEY_COL).reset_index(drop=True)
+	logger.info('df_train: {}'.format(df_train.shape))
+	df_test = pd.read_csv(TEST_FILE).sort_values(by=KEY_COL).reset_index(drop=True)
+	logger.info('df_test: {}'.format(df_test.shape))
 
-keys = df_test[KEY_COL]
-x_test = df_test.drop(columns=[KEY_COL])
-logger.info('x_test: {}'.format(x_test.shape))
-del df_test
+	logger.info('--- X-Y split ---')
+	x_train = df_train.drop(columns=[KEY_COL, TGT_COL])
+	logger.info('x_train: {}'.format(x_train.shape))
+	y_train = df_train[TGT_COL]
+	logger.info('y_train: {}'.format(y_train.shape))
+	del df_train
 
-# logger.info('--- reduce memory usage ---')
-# x_train = utils.reduce_mem_usage(x_train, logger)
-# x_test = utils.reduce_mem_usage(x_test, logger)
-# gc.collect()
+	keys = df_test[KEY_COL]
+	x_test = df_test.drop(columns=[KEY_COL])
+	logger.info('x_test: {}'.format(x_test.shape))
+	del df_test
 
-logger.info('--- cross validation ---')
-epochs = 20
-batch_size = 1000
-clf = KerasClassifier(build_fn=gen_model, epochs=epochs, batch_size=batch_size)
-kfold = StratifiedKFold(n_splits=5, shuffle=True, random_state=0)
-cv_auc = cross_val_score(clf, x_train, y_train, cv=kfold, scoring='roc_auc', verbose=3)
-logger.info('auc of each cv: {}'.format(cv_auc))
-mean_auc = mean(cv_auc)
-logger.info('mean auc: {}'.format(mean_auc))
+	# logger.info('--- reduce memory usage ---')
+	# x_train = utils.reduce_mem_usage(x_train, logger)
+	# x_test = utils.reduce_mem_usage(x_test, logger)
+	# gc.collect()
 
-logger.info('fitting to train data...')
-clf.fit(x_train, y_train)
+	logger.info('--- cross validation ---')
+	epochs = 20
+	batch_size = 1000
+	clf = KerasClassifier(build_fn=gen_model, epochs=epochs, batch_size=batch_size)
+	kfold = StratifiedKFold(n_splits=5, shuffle=True, random_state=0)
+	cv_auc = cross_val_score(clf, x_train, y_train, cv=kfold, scoring='roc_auc', verbose=3)
+	logger.info('auc of each cv: {}'.format(cv_auc))
+	mean_auc = mean(cv_auc)
+	logger.info('mean auc: {}'.format(mean_auc))
 
-logger.info('predicting test data...')
-pred_test = clf.predict_proba(x_test, batch_size=1000, verbose=1)[:, 1]
+	logger.info('fitting to train data...')
+	clf.fit(x_train, y_train)
 
-logger.info('--- save submission file ---')
-df_submission = pd.DataFrame({
-                            KEY_COL: keys,
-                            TGT_COL: pred_test
-                        })
-logger.info('saving: ' + SUBMISSION_FILE)
-df_submission.to_csv(SUBMISSION_FILE, index=False)
+	logger.info('predicting test data...')
+	pred_test = clf.predict_proba(x_test, batch_size=1000, verbose=1)[:, 1]
 
-logger.info('=== end nn.py ===')
+	logger.info('--- save submission file ---')
+	df_submission = pd.DataFrame({
+	                            KEY_COL: keys,
+	                            TGT_COL: pred_test
+	                        })
+	logger.info('saving: ' + SUBMISSION_FILE)
+	df_submission.to_csv(SUBMISSION_FILE, index=False)
+
+	logger.info('=== end nn.py ===')
 
